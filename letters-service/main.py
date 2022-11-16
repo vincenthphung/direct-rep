53d6a1a7c6315f282d7c6e23a7f2e9a9fc128b95
John <john.gardner823@gmail.com>
2022-11-15 20:16:34 -0600

    Created FastAPI microservices for Users and Letters. Edited Docker files to integrate microservices and PostgreSQL database.

---
+++ letters-service/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import os
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        os.environ.get("CORS_HOST", "http://localhost:3000")
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/letters")
+def launch_letters():
+    return {"letters-service/main.py"}
---
+++ users-service/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import os
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        os.environ.get("CORS_HOST", "http://localhost:3000")
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/users")
+def launch_users():
+    return {"users-service/main.py"}
