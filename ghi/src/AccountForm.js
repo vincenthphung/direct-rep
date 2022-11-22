6e598018c02cfd7de147d01d8297c3ed8a10fad0
Aaryan Mittal <aaryanmittal154@gmail.com>
2022-11-22 07:15:59 +0530

    Need to finish User form

--- ghi/src/AccountForm.js
+++ ghi/src/AccountForm.js
@@ -1,4 +1,52 @@
 import { useState } from "react";
+import React from "react";
+
+class SignUpForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullName: "",
+      email: "",
+      zipcode: "",
+      password: ""
+    };
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleEmailChange = this.handleEmailChnage.bind(this);
+    this.handleZipcodeChange = this.handleZipcodeChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleNameChange(event){
+    const value = event.target.value;
+    this.setState({fullName: value});
+  }
+
+  handleEmailChange(event){
+    const value = event.target.value;
+    this.setEmail({email: value})
+  }
+
+  handleZipcodeChange(event){
+    const value = event.target.value;
+    this.setState({zipcode: value});
+  }
+
+  handlePasswordChange(event){
+    const value = event.target.value;
+    this.setPassword({password: value})
+  }
+
+  async handleSubmit(event){
+    event.preventDefault();
+    const data = {.....this.state};
+    data.full_name = data.fullName
+    delete data.fullName
+
+    const url = ""
+  }
+
+}
 
 function InputLabel(props) {
   const { id, placeholder, labelText, value, onChange, type } = props;
