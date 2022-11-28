136c11e7e8e562d7d99a2a6fff5115fedd300a3d
Aaryan Mittal <aaryanmittal154@gmail.com>
2022-11-29 05:15:14 +0530

    Worked on React Navbar, Dashboard

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -5,6 +5,10 @@ import ErrorNotification from "./ErrorNotification";
 import AccountForm from "./AccountForm";
 import RepForm from "./RepForm";
 import "./App.css";
+import Navigation from "./Nav";
+import Dashboard from "./Dashboard";
+import LetterForm from "./CreateLetter";
+import EditLetter from "./EditLetter";
 
 function App() {
   const [launch_info, setLaunchInfo] = useState([]);
@@ -13,7 +17,7 @@ function App() {
   useEffect(() => {
     async function getData() {
       let url = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/launch-details`;
-      console.log('fastapi url: ', url);
+      console.log("fastapi url: ", url);
       let response = await fetch(url);
       console.log("------- hello? -------");
       let data = await response.json();
@@ -27,14 +31,19 @@ function App() {
       }
     }
     getData();
-  }, [])
+  }, []);
 
   return (
     <div>
       <BrowserRouter>
+        <Navigation />
         <Routes>
           <Route path="/signup" element={<AccountForm />} />
           <Route path="/create/reps" element={<RepForm />} />
+          <Route path="/signup" element={<AccountForm />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/cletter" element={<LetterForm />} />
+          <Route path="/eletter" element={<EditLetter />} />
         </Routes>
       </BrowserRouter>
       <ErrorNotification error={error} />
---
+++ ghi/src/EditLetter.js
@@ -0,0 +1,3 @@
+export const EditLetter = () => {};
+
+export default EditLetter;
