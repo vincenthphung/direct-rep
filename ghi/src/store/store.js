5e68efeaca3a78ceb6f1af073510339d0ae6b0ef
Aaryan Mittal <aaryanmittal154@gmail.com>
2022-12-02 05:32:03 +0530

    React Endpoints connected with auth

--- ghi/src/store/store.js
+++ ghi/src/store/store.js
@@ -3,18 +3,21 @@ import { setupListeners } from "@reduxjs/toolkit/query";
 import { usersApi } from "./usersApi";
 import { repsApi } from "./repsApi";
 import { lettersApi } from "./lettersApi";
+import { authApi } from "./authApi";
 
 export const store = configureStore({
   reducer: {
     [usersApi.reducerPath]: usersApi.reducer,
     [repsApi.reducerPath]: repsApi.reducer,
     [lettersApi.reducerPath]: lettersApi.reducer,
+    [authApi.reducerPath]: authApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(usersApi.middleware)
       .concat(repsApi.middleware)
-      .concat(lettersApi.middleware),
+      .concat(lettersApi.middleware)
+      .concat(authApi.middleware),
 });
 
 setupListeners(store.dispatch);
