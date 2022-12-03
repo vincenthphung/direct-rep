7df9289857e15c4fe1246fa9da008258391ae369
John <john.gardner823@gmail.com>
2022-12-02 21:26:12 -0600

    tryin....

--- letters-service/queries/pool.py
+++ letters-service/queries/pool.py
@@ -1,4 +1,6 @@
 import os
 from psycopg_pool import ConnectionPool
 
-pool = ConnectionPool(conninfo=os.environ["DATABASE_URL"])
+DATABASE_URL = os.environ["DATABASE_URL"]
+
+pool = ConnectionPool(conninfo=DATABASE_URL)
