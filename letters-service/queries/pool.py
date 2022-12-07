4cdf1146fabfdf8d90ba2812fea17a227fc1739b
John <john.gardner823@gmail.com>
2022-12-06 23:16:55 -0600

    changed psycopg from getting connection-pool to just getting connection

--- letters-service/queries/pool.py
+++ letters-service/queries/pool.py
@@ -1,7 +1,9 @@
 import os
-from psycopg_pool import ConnectionPool
+# from psycopg_pool import ConnectionPool
+import psycopg
 
 LETTERS_DATABASE_URL = os.environ.get("LETTERS_DATABASE_URL")
 
 # pool = ConnectionPool(conninfo=os.environ["DATABASE_URL"])
-pool = ConnectionPool(conninfo=LETTERS_DATABASE_URL)
+# pool = ConnectionPool(conninfo=LETTERS_DATABASE_URL)
+conn = psycopg.connect(LETTERS_DATABASE_URL)
--- users-service/queries/pool.py
+++ users-service/queries/pool.py
@@ -1,7 +1,9 @@
 import os
-from psycopg_pool import ConnectionPool
+# from psycopg_pool import ConnectionPool
+import pscyopg
 
 USERS_DATABASE_URL = os.environ.get("USERS_DATABASE_URL")
 
 # pool = ConnectionPool(conninfo=os.environ["DATABASE_URL"])
-pool = ConnectionPool(conninfo=USERS_DATABASE_URL)
+# pool = ConnectionPool(conninfo=USERS_DATABASE_URL)
+conn = psycopg.connect(USERS_DATABASE_URL)
