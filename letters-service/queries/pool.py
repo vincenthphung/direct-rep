0fbb82d435702efb4ae757b0dd61620963f64ad4
John <john.gardner823@gmail.com>
2022-12-07 12:53:13 -0600

    testing keepalive

--- letters-service/queries/pool.py
+++ letters-service/queries/pool.py
@@ -2,8 +2,15 @@ import os
 # from psycopg_pool import ConnectionPool
 import psycopg
 
+keepalive_kwargs = {
+    "keepalives": 1,
+    "keepalives_idle": 60,
+    "keepalives_interval": 10,
+    "keepalives_count": 5
+}
+
 LETTERS_DATABASE_URL = os.environ.get("LETTERS_DATABASE_URL")
 
 # pool = ConnectionPool(conninfo=os.environ["DATABASE_URL"])
 # pool = ConnectionPool(conninfo=LETTERS_DATABASE_URL)
-conn = psycopg.connect(LETTERS_DATABASE_URL)
+conn = psycopg.connect(conninfo=LETTERS_DATABASE_URL, **keepalive_kwargs)
