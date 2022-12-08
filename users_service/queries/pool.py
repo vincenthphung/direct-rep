6b9a4b18c07d68b3d1cead63d85055a9b9262b93
Aaryan Mittal <aaryanmittal154@gmail.com>
2022-12-08 09:14:56 +0530

    removed commit and added print statements

--- users_service/queries/pool.py
+++ users_service/queries/pool.py
@@ -1,6 +1,6 @@
 import os
-# from psycopg_pool import ConnectionPool
-import psycopg
+from psycopg_pool import ConnectionPool
+# import psycopg
 
 keepalive_kwargs = {
     "keepalives": 1,
@@ -13,6 +13,6 @@ USERS_DATABASE_URL = os.environ.get("USERS_DATABASE_URL")
 print("\n\n\n\n######\nUSERS_DATABASE_URL ===> ", USERS_DATABASE_URL)
 
 # pool = ConnectionPool(conninfo=os.environ["DATABASE_URL"])
-# pool = ConnectionPool(conninfo=USERS_DATABASE_URL)
+pool = ConnectionPool(conninfo=USERS_DATABASE_URL)
 # conn = psycopg.connect(USERS_DATABASE_URL)
-conn = psycopg.connect(conninfo=USERS_DATABASE_URL, **keepalive_kwargs)
+# conn = psycopg.connect(conninfo=USERS_DATABASE_URL, **keepalive_kwargs)
