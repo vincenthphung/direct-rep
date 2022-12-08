30386d8e9a1256d4c3129a7218ad7e04bce25f06
Aaryan Mittal <aaryanmittal154@gmail.com>
2022-12-08 09:23:53 +0530

    copied from old gitlab branch

--- users_service/queries/accounts.py
+++ users_service/queries/accounts.py
@@ -2,12 +2,10 @@ from pydantic import BaseModel
 from typing import Optional, List, Union
 from datetime import date
 from queries.pool import pool
-# from queries.pool import conn
 
 
 class Error(BaseModel):
-    message: str
-
+  message: str
 
 class Account(BaseModel):
     id: int
@@ -16,13 +14,11 @@ class Account(BaseModel):
     zipcode: int
     hashed_password: str
 
-
 class AccountIn(BaseModel):
     full_name: str
     email: str
     zipcode: int
     password: str
-
 
 class AccountOut(BaseModel):
     id: int
@@ -35,9 +31,8 @@ class AccountRepo:
     def get(self, email: str) -> Optional[Account]:
         # connect the database
         with pool.connection() as conn:
-        # get a cursor (something to run SQL with)
+            # get a cursor (something to run SQL with)
             with conn.cursor() as db:
-                print("\n\n\n\n######\nCONN.CURSOR() ===> ", db)
                 # Run our SELECT statement
                 result = db.execute(
                     """
@@ -51,9 +46,7 @@ class AccountRepo:
                     """,
                     [email]
                 )
-                print("\n\n\n\n######\nRESULT ===> ", result)
                 record = result.fetchone()
-                print("\n\n\n\n######\nRECORD ===> ", record)
                 if record is None:
                     return None
 
@@ -68,10 +61,8 @@ class AccountRepo:
     def create(self, account: AccountIn, hashed_password: str) -> Account:
         # connect the database
         with pool.connection() as conn:
-            print("\n\n\n\n######\nCREATE conn ===> ", conn)
-        # get a cursor (something to run SQL with)
+            # get a cursor (something to run SQL with)
             with conn.cursor() as db:
-                print("\n\n\n\n######\nCREATE db ===> ", db)
                 # Run our INSERT statement
                 result = db.execute(
                     """
@@ -79,7 +70,7 @@ class AccountRepo:
                         (full_name, email, zipcode, hashed_password)
                     VALUES
                         (%s, %s, %s, %s)
-                    RETURNING id; commit;
+                    RETURNING id;
                     """,
                     [
                         account.full_name,
@@ -89,7 +80,6 @@ class AccountRepo:
                     ]
                 )
                 id = result.fetchone()[0]
-                print("\n\n\n\n######\nCREATE id ===> ", id, account.email)
                 return Account(
                     id=id,
                     full_name=account.full_name,
@@ -97,36 +87,34 @@ class AccountRepo:
                     zipcode=account.zipcode,
                     hashed_password=hashed_password
                 )
-
-
     def update(self, id: int, account: AccountIn, hashed_password: str) -> Union[Account, Error]:
         try:
-            # connect to the database
+        # connect to the database
             with pool.connection() as conn:
-            # get a cursor (something to run SQL with)
+        # get a cursor (something to run SQL with)
                 with conn.cursor() as db:
                     db.execute(
-                        """
+                    """
                     UPDATE users
                     SET full_name = %s, email = %s, zipcode = %s, hashed_password = %s
                     WHERE id = %s
                     """,
-                        [
-                            account.full_name,
-                            account.email,
-                            account.zipcode,
-                            hashed_password,
-                            id
-                        ]
-                    )
+                    [
+                        account.full_name,
+                        account.email,
+                        account.zipcode,
+                        hashed_password,
+                        id
+                    ]
+                )
 
                 # old_data = letter.dict()
                 return Account(id=id,
-                            full_name=account.full_name,
-                            email=account.email,
-                            zipcode=account.zipcode,
-                            hashed_password=hashed_password)
+                                full_name=account.full_name,
+                                email=account.email,
+                                zipcode=account.zipcode,
+                                hashed_password=hashed_password)
 
         except Exception as e:
             print(e)
-            return {"message": "could not update account"}
+            return{"message": "could not update account"}
