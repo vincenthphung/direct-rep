c1601b6ac083251ae8daf75f2a19215595911c53
Aaryan Mittal <aaryanmittal154@gmail.com>
2022-12-08 08:58:42 +0530

    Went back to the old method

--- letters_service/queries/letters.py
+++ letters_service/queries/letters.py
@@ -1,8 +1,8 @@
 from pydantic import BaseModel
 from datetime import datetime
 from typing import Optional, Union, List
-# from queries.pool import pool
-from queries.pool import conn
+from queries.pool import pool
+# from queries.pool import conn
 
 
 class Error(BaseModel):
@@ -47,54 +47,54 @@ class Issue(BaseModel):
 class LetterRepository:
     def create(self, topic: str, stance: bool, content: str, user_id: int) -> Union[LetterOut, Error]:
         try:
-            # with pool.connection() as conn:
-            with conn.cursor() as db:
-                result = db.execute(
-                    """
-                    INSERT INTO letter
-                        (topic, stance, content, user_id)
-                    VALUES
-                        (%s, %s, %s, %s)
-                    RETURNING id;
-                    """,
-                    [
-                        topic,
-                        stance,
-                        content,
-                        user_id
-                    ]
-                )
-                id = result.fetchone()[0]
-                # print("Check id \n \n", id)
-                return LetterNew(
-                    id=id,
-                    topic=topic,
-                    stance=stance,
-                    content=content,
-                    user_id=user_id
-                )
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        INSERT INTO letter
+                            (topic, stance, content, user_id)
+                        VALUES
+                            (%s, %s, %s, %s)
+                        RETURNING id;
+                        """,
+                        [
+                            topic,
+                            stance,
+                            content,
+                            user_id
+                        ]
+                    )
+                    id = result.fetchone()[0]
+                    # print("Check id \n \n", id)
+                    return LetterNew(
+                        id=id,
+                        topic=topic,
+                        stance=stance,
+                        content=content,
+                        user_id=user_id
+                    )
         except Exception:
             return {"message": "Create letter did not work"}
 
     def update(self, letter_id: int, content: str) -> Union[LetterUpdate, Error]:
         try:
             # connect to the database
-            # with pool.connection() as conn:
+            with pool.connection() as conn:
             # get a cursor (something to run SQL with)
-            with conn.cursor() as db:
-                db.execute(
-                    """
-                UPDATE letter
-                SET content = %s
-                WHERE id = %s
-                """,
-                    [
-                        content,
-                        letter_id
-                    ]
-                )
-            # old_data = letter.dict()
-            return LetterUpdate(id=letter_id, content=content)
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                    UPDATE letter
+                    SET content = %s
+                    WHERE id = %s
+                    """,
+                        [
+                            content,
+                            letter_id
+                        ]
+                    )
+                # old_data = letter.dict()
+                return LetterUpdate(id=letter_id, content=content)
         except Exception as e:
             print(e)
             return {"message": "could not update letter"}
@@ -102,30 +102,30 @@ class LetterRepository:
     def get_all(self) -> Union[Error, List[LetterOut]]:
         try:
             # connect to the database
-            # with pool.connection() as conn:
+            with pool.connection() as conn:
             # get a cursor (something to run SQL with)
-            with conn.cursor() as db:
-                # run our SELECT statement
-                result = db.execute(
-                    """
-                    SELECT id, created, topic, stance, content, user_id
-                    FROM letter
-                    ORDER BY id;
-                    """
-                )
-                result = []
-                for record in db:
-                    # print("\n \n record get all", record)
-                    letter = LetterOut(
-                        id=record[0],
-                        created=record[1],
-                        topic=record[2],
-                        stance=record[3],
-                        content=record[4],
-                        user_id=record[5]
+                with conn.cursor() as db:
+                    # run our SELECT statement
+                    result = db.execute(
+                        """
+                        SELECT id, created, topic, stance, content, user_id
+                        FROM letter
+                        ORDER BY id;
+                        """
                     )
-                    result.append(letter)
-                return result
+                    result = []
+                    for record in db:
+                        # print("\n \n record get all", record)
+                        letter = LetterOut(
+                            id=record[0],
+                            created=record[1],
+                            topic=record[2],
+                            stance=record[3],
+                            content=record[4],
+                            user_id=record[5]
+                        )
+                        result.append(letter)
+                    return result
         except Exception as e:
             print("ERROR")
             print(e)
@@ -134,37 +134,37 @@ class LetterRepository:
     def get_one(self, letter_id: int) -> Optional[LetterOut]:
         try:
             # connect to the database
-            # with pool.connection() as conn:
+            with pool.connection() as conn:
             # get a cursor (something to run SQL with)
-            with conn.cursor() as db:
-                result = db.execute(
-                    """
-                    SELECT id, created, topic, stance, content, user_id
-                    FROM letter
-                    WHERE id = %s
-                    """,
-                    [letter_id]
-                )
-                record = result.fetchone()
-                if record is None:
-                    return None
-                return self.record_to_letter_out(record)
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        SELECT id, created, topic, stance, content, user_id
+                        FROM letter
+                        WHERE id = %s
+                        """,
+                        [letter_id]
+                    )
+                    record = result.fetchone()
+                    if record is None:
+                        return None
+                    return self.record_to_letter_out(record)
         except Exception as e:
             print(e)
             return {"message": "could not get that letter"}
 
     def delete(self, letter_id: int) -> bool:
         try:
-            # with pool.connection() as conn:
-            with conn.cursor() as db:
-                db.execute(
-                    """
-                    DELETE FROM letter
-                    WHERE id = %s
-                    """,
-                    [letter_id]
-                )
-                return True
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        DELETE FROM letter
+                        WHERE id = %s
+                        """,
+                        [letter_id]
+                    )
+                    return True
         except Exception as e:
             print(e)
             return False
@@ -183,24 +183,24 @@ class LetterRepository:
 class IssueRepository:
     def get_all(self) -> Union[Error, List[Issue]]:
         try:
-            # with pool.connection() as conn:
-            with conn.cursor() as db:
-                result = db.execute(
-                    """
-                    SELECT id, user_issue, openai_issue
-                    FROM issue
-                    ORDER BY id;
-                    """
-                )
-                result = []
-                for record in db:
-                    issue = Issue(
-                        id=record[0],
-                        user_issue=record[1],
-                        openai_issue=record[2]
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        SELECT id, user_issue, openai_issue
+                        FROM issue
+                        ORDER BY id;
+                        """
                     )
-                    result.append(issue)
-                return result
+                    result = []
+                    for record in db:
+                        issue = Issue(
+                            id=record[0],
+                            user_issue=record[1],
+                            openai_issue=record[2]
+                        )
+                        result.append(issue)
+                    return result
         except Exception as e:
             print("ERROR")
             print(e)
--- letters_service/queries/reps.py
+++ letters_service/queries/reps.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 from typing import Optional, Union, List
-# from queries.pool import pool
-from queries.pool import conn
+from queries.pool import pool
+# from queries.pool import conn
 
 
 class Error(BaseModel):
@@ -41,40 +41,40 @@ class RepOut(BaseModel):
 class RepRepository(BaseModel):
     def create(self, rep: RepIn) -> Union[RepOut, Error]:
         try:
-            # with pool.connection() as conn:
-            with conn.cursor() as db:
-                result = db.execute(
-                    """
-                    INSERT INTO rep
-                        (office, level, name, party, address, email, letter_id)
-                    VALUES
-                        (%s, %s, %s, %s, %s, %s, %s)
-                    RETURNING rep_id;
-                    """,
-                    [
-                        rep.office,
-                        rep.level,
-                        rep.name,
-                        rep.party,
-                        rep.address,
-                        rep.email,
-                        rep.letter_id
-                    ]
-                )
-                rep_id = result.fetchone()[0]
-                # print("\n \n Check id \n \n", rep_id)
-                # print("\n \n Check rep \n \n", RepOut)
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        INSERT INTO rep
+                            (office, level, name, party, address, email, letter_id)
+                        VALUES
+                            (%s, %s, %s, %s, %s, %s, %s)
+                        RETURNING rep_id;
+                        """,
+                        [
+                            rep.office,
+                            rep.level,
+                            rep.name,
+                            rep.party,
+                            rep.address,
+                            rep.email,
+                            rep.letter_id
+                        ]
+                    )
+                    rep_id = result.fetchone()[0]
+                    # print("\n \n Check id \n \n", rep_id)
+                    # print("\n \n Check rep \n \n", RepOut)
 
-                return RepOut(
-                    rep_id=rep_id,
-                    office=rep.office,
-                    level=rep.level,
-                    name=rep.name,
-                    party=rep.party,
-                    address=rep.address,
-                    email=rep.email,
-                    letter_id=rep.letter_id
-                )
+                    return RepOut(
+                        rep_id=rep_id,
+                        office=rep.office,
+                        level=rep.level,
+                        name=rep.name,
+                        party=rep.party,
+                        address=rep.address,
+                        email=rep.email,
+                        letter_id=rep.letter_id
+                    )
         except Exception:
             print("ERROR")
             return {"message": "Create rep did not work"}
@@ -82,31 +82,31 @@ class RepRepository(BaseModel):
     def get_all(self) -> Union[Error, List[RepOut]]:
         try:
             # connect to the database
-            # with pool.connection() as conn:
+            with pool.connection() as conn:
             # get a cursor (something to run SQL with)
-            with conn.cursor() as db:
-                # run our SELECT statement
-                result = db.execute(
-                    """
-                    SELECT rep_id, office, level, name, party, address, email, letter_id
-                    FROM rep
-                    ORDER BY rep_id;
-                    """
-                )
-                result = []
-                for record in db:
-                    rep = RepOut(
-                        rep_id=record[0],
-                        office=record[1],
-                        level=record[2],
-                        name=record[3],
-                        party=record[4],
-                        address=record[5],
-                        email=record[6],
-                        letter_id=record[7]
+                with conn.cursor() as db:
+                    # run our SELECT statement
+                    result = db.execute(
+                        """
+                        SELECT rep_id, office, level, name, party, address, email, letter_id
+                        FROM rep
+                        ORDER BY rep_id;
+                        """
                     )
-                    result.append(rep)
-                return result
+                    result = []
+                    for record in db:
+                        rep = RepOut(
+                            rep_id=record[0],
+                            office=record[1],
+                            level=record[2],
+                            name=record[3],
+                            party=record[4],
+                            address=record[5],
+                            email=record[6],
+                            letter_id=record[7]
+                        )
+                        result.append(rep)
+                    return result
         except Exception as e:
             print("ERROR")
             print(e)
@@ -115,21 +115,21 @@ class RepRepository(BaseModel):
     def get_one(self, rep_id: int) -> Optional[RepOut]:
         try:
             # connect to the database
-            # with pool.connection() as conn:
+            with pool.connection() as conn:
             # get a cursor (something to run SQL with)
-            with conn.cursor() as db:
-                result = db.execute(
-                    """
-                    SELECT rep_id, office, level, name, party, address, email, letter_id
-                    FROM rep
-                    WHERE rep_id = %s
-                    """,
-                    [rep_id]
-                )
-                record = result.fetchone()
-                if record is None:
-                    return None
-                return self.record_to_rep_out(record)
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        SELECT rep_id, office, level, name, party, address, email, letter_id
+                        FROM rep
+                        WHERE rep_id = %s
+                        """,
+                        [rep_id]
+                    )
+                    record = result.fetchone()
+                    if record is None:
+                        return None
+                    return self.record_to_rep_out(record)
 
         except Exception as e:
             print(e)
@@ -150,32 +150,32 @@ class RepRepository(BaseModel):
     def get_per_letter(self, letter_id: int) -> Union[List[RepOut], Error]:
         try:
             # connect to the database
-            # with pool.connection() as conn:
+            with pool.connection() as conn:
             # get a cursor (something to run SQL with)
-            with conn.cursor() as db:
-                result = db.execute(
-                    """
-                    SELECT rep_id, office, level, name, party, address, email, letter_id
-                    FROM rep
-                    WHERE letter_id = %s
-                    """,
-                    [letter_id]
-                )
-                result = []
-                for record in db:
-                    rep = RepOut(
-                        rep_id=record[0],
-                        office=record[1],
-                        level=record[2],
-                        name=record[3],
-                        party=record[4],
-                        address=record[5],
-                        email=record[6],
-                        letter_id=record[7]
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        SELECT rep_id, office, level, name, party, address, email, letter_id
+                        FROM rep
+                        WHERE letter_id = %s
+                        """,
+                        [letter_id]
                     )
-                    result.append(rep)
-                    # print("REP PER LETTER", rep)
-                return result
+                    result = []
+                    for record in db:
+                        rep = RepOut(
+                            rep_id=record[0],
+                            office=record[1],
+                            level=record[2],
+                            name=record[3],
+                            party=record[4],
+                            address=record[5],
+                            email=record[6],
+                            letter_id=record[7]
+                        )
+                        result.append(rep)
+                        # print("REP PER LETTER", rep)
+                    return result
         except Exception as e:
             print("ERROR")
             print(e)
@@ -183,16 +183,16 @@ class RepRepository(BaseModel):
 
     def delete(self, letter_id: int, rep_id: int) -> bool:
         try:
-            # with pool.connection() as conn:
-            with conn.cursor() as db:
-                db.execute(
-                    """
-                    DELETE FROM rep
-                    WHERE letter_id = %s AND rep_id = %s
-                    """,
-                    [letter_id, rep_id]
-                )
-                return True
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        DELETE FROM rep
+                        WHERE letter_id = %s AND rep_id = %s
+                        """,
+                        [letter_id, rep_id]
+                    )
+                    return True
         except Exception as e:
             print(e)
             return False
