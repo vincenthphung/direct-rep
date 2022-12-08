5a665d8c751e47a4e04e6e828f3c90227d586340
Aaryan Mittal <aaryanmittal154@gmail.com>
2022-12-08 10:14:40 +0530

    updated the pool

--- letters_service/queries/pool.py
+++ letters_service/queries/pool.py
@@ -2,15 +2,16 @@ import os
 # from psycopg_pool import ConnectionPool
 import psycopg
 
-keepalive_kwargs = {
-    "keepalives": 1,
-    "keepalives_idle": 300,
-    "keepalives_interval": 10,
-    "keepalives_count": 5
-}
+# keepalive_kwargs = {
+#     "keepalives": 1,
+#     "keepalives_idle": 300,
+#     "keepalives_interval": 10,
+#     "keepalives_count": 5
+# }
 
 LETTERS_DATABASE_URL = os.environ.get("LETTERS_DATABASE_URL")
 
 # pool = ConnectionPool(conninfo=os.environ["DATABASE_URL"])
 # pool = ConnectionPool(conninfo=LETTERS_DATABASE_URL)
-conn = psycopg.connect(conninfo=LETTERS_DATABASE_URL, **keepalive_kwargs)
+# conn = psycopg.connect(conninfo=LETTERS_DATABASE_URL, **keepalive_kwargs)
+conn = psycopg.connect(LETTERS_DATABASE_URL)
