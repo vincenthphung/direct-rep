8bb781529f011da0b07162e27d130734f70b71a3
Kim Geraghty <kim.geraghty.kg@gmail.com>
2022-12-09 14:09:59 -0800

    testing issues

--- letters_service/tests/test_letters_service.py
+++ letters_service/tests/test_letters_service.py
@@ -1,11 +1,29 @@
 from fastapi.testclient import TestClient
 from routers.letters import authenticator
-from queries.letters import LetterRepository
+# from queries.letters import LetterRepository
 from main import app
 
 client = TestClient(app)
 
 print("Letters test file")
+
+
+def test_get_issue():
+  issue = {
+    "id": 1,
+    "user_issue": "Healthcare Reform",
+    "openai_issue": "a single-payer healthcare system: ",
+  }
+  app.dependency_overrides[
+      authenticator.try_get_current_account_data
+  ] = lambda: issue
+  response = client.get("/api/issues")
+  app.dependency_overrides = {}
+  assert response.status_code == 200
+  data = response.json()
+  print("ISSUE test data", data)
+  assert data[0] == issue
+
 
 # def test_get_letter():
 #     letter = {
@@ -26,41 +44,46 @@ print("Letters test file")
 #     print("LETTER test data", data)
 #     assert data[0] == letter
 
-class CreateLetter:
-    def create(self, letter):
-        result = {"id": 1}
-        result.update(letter)
-        return result
 
 
-def test_create_letter():
-    # Arrange
-    app.dependency_overrides[LetterRepository] = CreateLetter
-    app.dependency_overrides[authenticator.try_get_current_account_data] = lambda: CreateLetter
+# class CreateLetter:
+#     def create(self, letter):
+#         result = {"id": 1, "created": "2008-09-15T15:53:00+05:00", "user_id": 1}
+#         result.update(letter)
+#         return result
 
-    json = {
-      "created": "2008-09-15T15:53:00+05:00",
-      "topic": "coolio",
-      "stance": True,
-      "content": "Life is good",
-      "user_id": 99,
-    }
+# topic = "coolio"
+# stance = True
 
-    expected = {
-      "id": 99,
-      "created": "2008-09-15T15:53:00+05:00",
-      "topic": "coolio",
-      "stance": True,
-      "content": "Life is good",
-      "user_id": 99,
-    }
+# def test_create_letter(topic, stance):
+#     # Arrange
+#     app.dependency_overrides[LetterRepository] = CreateLetter
+#     app.dependency_overrides[authenticator.try_get_current_account_data] = lambda: CreateLetter
 
-    # Act
-    response = client.post("/api/letters", json=json)
+#     data = {
+#       "id": int,
+#       "created": str,
+#       "topic": str,
+#       "stance": bool,
+#       "content": str,
+#       "user_id": int,
+#     }
 
-    # Assert
-    assert response.status_code == 200
-    assert response.json() == expected
+#     expected = {
+#       "id": int,
+#       "created": str,
+#       "topic": str,
+#       "stance": bool,
+#       "content": str,
+#       "user_id": int,
+#     }
 
-    # Clean up
-    app.dependency_overrides = {}
+#     # Act
+#     response = client.post("/api/letters", json=data)
+
+#     # Assert
+#     assert response.status_code == 200
+#     assert response.json() == expected
+
+#     # Clean up
+#     app.dependency_overrides = {}
