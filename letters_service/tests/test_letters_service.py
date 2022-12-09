54e130905307613099cd60848491dc578234c347
Kim Geraghty <kim.geraghty.kg@gmail.com>
2022-12-09 12:56:27 -0800

    CreateLetter test

--- letters_service/tests/test_letters_service.py
+++ letters_service/tests/test_letters_service.py
@@ -1,26 +1,66 @@
 from fastapi.testclient import TestClient
 from routers.letters import authenticator
+from queries.letters import LetterRepository
 from main import app
 
 client = TestClient(app)
 
 print("Letters test file")
 
+# def test_get_letter():
+#     letter = {
+#       "id": 99,
+#       "created": "2008-09-15T15:53:00+05:00",
+#       "topic": "coolio",
+#       "stance": True,
+#       "content": "Life is good",
+#       "user_id": 99,
+#     }
+#     app.dependency_overrides[
+#         authenticator.try_get_current_account_data
+#     ] = lambda: letter
+#     response = client.get("/api/letters")
+#     app.dependency_overrides = {}
+#     assert response.status_code == 200
+#     data = response.json()
+#     print("LETTER test data", data)
+#     assert data[0] == letter
+
+class CreateLetter:
+    def create(self, letter):
+        result = {"id": 1}
+        result.update(letter)
+        return result
+
+
 def test_create_letter():
-    letter = {
-      "id": 1,
+    # Arrange
+    app.dependency_overrides[LetterRepository] = CreateLetter
+    app.dependency_overrides[authenticator.try_get_current_account_data] = lambda: CreateLetter
+
+    json = {
       "created": "2008-09-15T15:53:00+05:00",
       "topic": "coolio",
       "stance": True,
       "content": "Life is good",
-      "user_id": 1,
+      "user_id": 99,
     }
-    app.dependency_overrides[
-        authenticator.try_get_current_account_data
-    ] = lambda: letter
-    response = client.get("/api/letters")
-    app.dependency_overrides = {}
+
+    expected = {
+      "id": 99,
+      "created": "2008-09-15T15:53:00+05:00",
+      "topic": "coolio",
+      "stance": True,
+      "content": "Life is good",
+      "user_id": 99,
+    }
+
+    # Act
+    response = client.post("/api/letters", json=json)
+
+    # Assert
     assert response.status_code == 200
-    data = response.json()
-    print("LETTER test data", data)
-    assert data[0] == letter
+    assert response.json() == expected
+
+    # Clean up
+    app.dependency_overrides = {}
