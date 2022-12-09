7baee5407224b44f10693f5a80e92f3736fb3013
Kim Geraghty <kim.geraghty.kg@gmail.com>
2022-12-09 15:42:25 -0800

    testing reps

--- letters_service/tests/test_letters_service.py
+++ letters_service/tests/test_letters_service.py
@@ -1,7 +1,7 @@
 from fastapi.testclient import TestClient
 from routers.letters import authenticator
 from queries.letters import LetterRepository
-# from queries.reps import RepRepository
+from queries.reps import RepRepository
 from main import app
 
 client = TestClient(app)
@@ -45,113 +45,48 @@ def test_get_all_letters():
     app.dependency_overrides = {}
 
 
-# class CreateRep:
-#     def create(self, rep):
-#         result = {"rep_id": 1}
-#         result.update(rep)
-#         return result
+class CreateRep:
+    def create(self, rep):
+        result = {"rep_id": 1}
+        result.update(rep)
+        return result
 
-# def test_create_rep():
-#     rep = {}
-#     print("Reps?, rep", rep)
-#     # Arrange
-#     app.dependency_overrides[RepRepository] = lambda: rep
-#     print("REPS? CreateRep", rep)
+def test_create_rep():
 
-#     app.dependency_overrides[authenticator.try_get_current_account_data] = lambda: rep
-#     print("REPS? dependency rep", rep)
-#     # app.dependency_overrides[get_current_user] = override_auth_user
-#     json = {
-#     "office": "President",
-#     "level": "country",
-#     "name": "Joe Biden",
-#     "party": "Democrat",
-#     "address": "123 White House Street",
-#     "email": "joe@biden.com",
-#     "letter_id": 1
-#     }
-#     expected = {
-#     "rep_id": int,
-#     "office": "President",
-#     "level": "country",
-#     "name": "Joe Biden",
-#     "party": "Democrat",
-#     "address": "123 White House Street",
-#     "email": "joe@biden.com",
-#     "letter_id": 1
-#     }
+    # Arrange
+    app.dependency_overrides[RepRepository] = CreateRep
 
-#     # Act
-#     response = client.post("/api/reps", json=json)
-#     print("REPS? response", response)
-#     # Assert
-#     assert response.status_code == 200
-#     print("REPS test data", response)
-#     assert response.json() == expected
+    app.dependency_overrides[authenticator.try_get_current_account_data] = lambda: CreateRep
+    # app.dependency_overrides[get_current_user] = override_auth_user
 
-#     # Clean up
-#     app.dependency_overrides = {}
+    json = CreateRep(
+    office="President",
+    level="country",
+    name= "Joe Biden",
+    party= "Democrat",
+    address= "123 White House Street",
+    email= "joe@biden.com",
+    letter_id= 1
+    ).dict()
 
+    expected = {
+    "rep_id": int,
+    "office": "President",
+    "level": "country",
+    "name": "Joe Biden",
+    "party": "Democrat",
+    "address": "123 White House Street",
+    "email": "joe@biden.com",
+    "letter_id": 1
+    }
 
-# def test_get_letter():
-#     letter = {
-#       "id": 99,
-#       "created": "2008-09-15T15:53:00+05:00",
-#       "topic": "coolio",
-#       "stance": True,
-#       "content": "Life is good",
-#       "user_id": 99,
-#     }
-#     app.dependency_overrides[
-#         authenticator.try_get_current_account_data
-#     ] = lambda: letter
-#     response = client.get("/api/letters")
-#     app.dependency_overrides = {}
-#     assert response.status_code == 200
-#     data = response.json()
-#     print("LETTER test data", data)
-#     assert data[0] == letter
+    # Act
+    response = client.post("/api/reps", json=json)
+    print("REPS? response", response)
+    # Assert
+    assert response.status_code == 200
+    print("REPS test data", response)
+    assert response.json() == expected
 
-
-
-# class CreateLetter:
-#     def create(self, letter):
-#         result = {"id": 1, "created": "2008-09-15T15:53:00+05:00", "user_id": 1}
-#         result.update(letter)
-#         return result
-
-# topic = "coolio"
-# stance = True
-
-# def test_create_letter(topic, stance):
-#     # Arrange
-#     app.dependency_overrides[LetterRepository] = CreateLetter
-#     app.dependency_overrides[authenticator.try_get_current_account_data] = lambda: CreateLetter
-
-#     data = {
-#       "id": int,
-#       "created": str,
-#       "topic": str,
-#       "stance": bool,
-#       "content": str,
-#       "user_id": int,
-#     }
-
-#     expected = {
-#       "id": int,
-#       "created": str,
-#       "topic": str,
-#       "stance": bool,
-#       "content": str,
-#       "user_id": int,
-#     }
-
-#     # Act
-#     response = client.post("/api/letters", json=data)
-
-#     # Assert
-#     assert response.status_code == 200
-#     assert response.json() == expected
-
-#     # Clean up
-#     app.dependency_overrides = {}
+    # Clean up
+    app.dependency_overrides = {}
