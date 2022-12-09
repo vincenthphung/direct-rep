de474d0fb2f1df1ed2146dd644f9ab81df0c0d79
Kim Geraghty <kim.geraghty.kg@gmail.com>
2022-12-09 12:10:52 -0800

    testing letters test

--- letters_service/tests/test_letters_service.py
+++ letters_service/tests/test_letters_service.py
@@ -4,6 +4,7 @@ from main import app
 
 client = TestClient(app)
 
+print("Letters test file")
 
 # def test_get_token_returns_none_for_user_not_logged_in():
 #     app.dependency_overrides[authenticator.try_get_current_account_data] = lambda: None
@@ -13,20 +14,20 @@ client = TestClient(app)
 #     assert response.json() == None
 
 
-def test_create_letter():
-    letter = {
-      "id": 1,
-      "created": "2008-09-15T15:53:00+05:00",
-      "topic": "coolio",
-      "stance": True,
-      "content": "Life is good",
-      "user_id": 1,
-    }
-    app.dependency_overrides[
-        authenticator.try_get_current_account_data
-    ] = lambda: letter
-    response = client.get("/api/letters")
-    app.dependency_overrides = {}
-    assert response.status_code == 200
-    data = response.json()
-    assert data["letter"] == letter
+# def test_create_letter():
+#     letter = {
+#       "id": 1,
+#       "created": "2008-09-15T15:53:00+05:00",
+#       "topic": "coolio",
+#       "stance": True,
+#       "content": "Life is good",
+#       "user_id": 1,
+#     }
+#     app.dependency_overrides[
+#         authenticator.try_get_current_account_data
+#     ] = lambda: letter
+#     response = client.get("/api/letters")
+#     app.dependency_overrides = {}
+#     assert response.status_code == 200
+#     data = response.json()
+#     assert data["letter"] == letter
