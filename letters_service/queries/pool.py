149d6bfe76c28d370814a78cecb6b97b5ba7c47a
Kim Geraghty <kim.geraghty.kg@gmail.com>
2022-12-09 12:33:00 -0800

    testing letters

--- letters_service/queries/pool.py
+++ letters_service/queries/pool.py
@@ -1,13 +1,6 @@
 import os
-from psycopg_pool import ConnectionPool
+# from psycopg_pool import ConnectionPool
 import psycopg
-
-# keepalive_kwargs = {
-#     "keepalives": 1,
-#     "keepalives_idle": 300,
-#     "keepalives_interval": 10,
-#     "keepalives_count": 5
-# }
 
 kwargs = {"autocommit": True}
 
@@ -15,7 +8,7 @@ LETTERS_DATABASE_URL = os.environ.get("LETTERS_DATABASE_URL")
 
 # pool = ConnectionPool(conninfo=os.environ["DATABASE_URL"])
 # pool = ConnectionPool(conninfo=LETTERS_DATABASE_URL)
-pool = ConnectionPool(conninfo=LETTERS_DATABASE_URL)
+# pool = ConnectionPool(conninfo=LETTERS_DATABASE_URL)
 # conn = psycopg.connect(conninfo=LETTERS_DATABASE_URL, **keepalive_kwargs)
 # conn = psycopg.connect(LETTERS_DATABASE_URL)
 conn = psycopg.connect(conninfo=LETTERS_DATABASE_URL, **kwargs)
--- letters_service/tests/test_letters_service.py
+++ letters_service/tests/test_letters_service.py
@@ -1,5 +1,8 @@
+import os
+# from psycopg_pool import ConnectionPool
+import psycopg
 from fastapi.testclient import TestClient
-# from routers.letters import authenticator
+from routers.letters import authenticator
 from main import app
 
 client = TestClient(app)
@@ -14,20 +17,20 @@ print("Letters test file")
 #     assert response.json() == None
 
 
-# def test_create_letter():
-#     letter = {
-#       "id": 1,
-#       "created": "2008-09-15T15:53:00+05:00",
-#       "topic": "coolio",
-#       "stance": True,
-#       "content": "Life is good",
-#       "user_id": 1,
-#     }
-#     app.dependency_overrides[
-#         authenticator.try_get_current_account_data
-#     ] = lambda: letter
-#     response = client.get("/api/letters")
-#     app.dependency_overrides = {}
-#     assert response.status_code == 200
-#     data = response.json()
-#     assert data["letter"] == letter
+def test_create_letter():
+    letter = {
+      "id": 1,
+      "created": "2008-09-15T15:53:00+05:00",
+      "topic": "coolio",
+      "stance": True,
+      "content": "Life is good",
+      "user_id": 1,
+    }
+    app.dependency_overrides[
+        authenticator.try_get_current_account_data
+    ] = lambda: letter
+    response = client.get("/api/letters")
+    app.dependency_overrides = {}
+    assert response.status_code == 200
+    data = response.json()
+    assert data["letter"] == letter
