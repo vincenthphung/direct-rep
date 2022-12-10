8cb2c6ed81a72e251bb0db20f4640ef846a49951
Kim Geraghty <kim.geraghty.kg@gmail.com>
2022-12-09 17:49:09 -0800

    lint again

--- letters_service/main.py
+++ letters_service/main.py
@@ -11,7 +11,7 @@ app.add_middleware(
         os.environ.get('CORS_HOST'),
         "http://localhost:3000",
         "https://directrep.gitlab.io"
-                ],
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
