c89f46ea37b1749654128ee370ca8d885e613382
Kim Geraghty <kim.geraghty.kg@gmail.com>
2022-12-09 17:38:16 -0800

    checking lint checks

--- letters_service/main.py
+++ letters_service/main.py
@@ -8,8 +8,10 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        os.environ.get('CORS_HOST'), "http://localhost:3000", "https://directrep.gitlab.io"
-        ],
+        os.environ.get('CORS_HOST'),
+        "http://localhost:3000",
+        "https://directrep.gitlab.io"
+                ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
