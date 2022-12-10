6a501b497d25b1907aa21d101e6041f567e8a738
Aaryan Mittal <aaryanmittal154@gmail.com>
2022-12-10 09:26:48 +0530

    Updated Navbar and landing page

--- ghi/src/Landing.js
+++ ghi/src/Landing.js
@@ -1,24 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
 function LandingPage() {
   return (
-    <div classNameName="jumbotron centered">
-      <div classNameName="container offset-3 col-6 mb-3">
-        <i classNameName="fas fa-key fa-6x" />
-        <h1 classNameName="display-3">Direct Rep</h1>
-        <p classNameName="lead">
+    <div className="jumbotron centered">
+      <div className="container offset-3 col-6 mb-3">
+        <i className="fas fa-key fa-6x" />
+        <h1 className="display-3">Direct Rep</h1>
+        <p className="lead">
           Get in touch with your representatives and let AI help you write a
           letter about important political issues.
         </p>
         <hr />
         <Link to="/signup">
-          <button type="submit" classNameName="btn btn-dark btn-lg">
+          <button type="submit" className="btn btn-dark btn-lg">
             Signup
           </button>
         </Link>
         <Link to="/login">
-          <button type="submit" classNameName="btn btn-dark btn-lg ms-5">
+          <button type="submit" className="btn btn-dark btn-lg ms-5">
             Login
           </button>
         </Link>
